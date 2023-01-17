9cebabc73f93413c5a9e0503ec39170cfe050558
Maria Jose Casamayor Polo <mcasamayorpolo@uoc.edu>
2023-01-17 19:51:07 +0100

    Thumbail lazyframes

--- src/app.js
+++ src/app.js
@@ -80,7 +80,7 @@ function loadInfoCharacter(characterId){
             containerVideo.setAttribute('data-vendor','youtube');
             containerVideo.setAttribute('data-src', l.video);
             containerVideo.setAttribute('data-autoplay', false);
-            containerVideo.setAttribute('data-thumbnail', './img/hqdefault.jpg');
+            containerVideo.setAttribute('data-thumbnail', 'hqdefault.jpg');
             lazyframe(containerVideo,{
                 lazyload: true,
                 autoplay: false
